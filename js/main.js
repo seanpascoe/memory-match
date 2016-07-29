c1490c32a34260ac20c6991b0eabd2776577ef7f
spflow <sean.pascoe@gmail.com>
2016-07-28 20:55:50 -0600

    begin card matching logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 var cards = [1, 1, 2, 2, 3, 3, 4, 4];
+var choice1;
+var choice2;
 
 function cardSelect(e) {
   var numEl = e.firstElementChild
-  numEl.style.visibility = "hidden";
+  var cardValue = numEl.innerHTML;
+  populateChoice(cardValue);
+
+  numEl.style.visibility = "visible";
+}
+
+function populateChoice(card) {
+
+  if (!choice1) {
+    choice1 = card;
+  } else {
+    choice2 = card;
+    cardCompare(choice1, choice2);
+  }
+  console.log(choice1);
+  console.log(choice2);
+}
+
+function cardCompare(ch1, ch2) {
+  if (ch1 === ch2) {
+    console.log("Cards Equal!")
+    resetChoices();
+    //keep cards revealed
+    //reset choices
+  } else {
+    console.log("Cards Not Equal!")
+    resetChoices();
+    //hide cards again
+    //reset choices to null
+  }
+}
+
+function resetChoices() {
+  choice1 = "";
+  choice2 = "";
 }
