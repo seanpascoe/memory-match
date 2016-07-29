7f01e5b4f24929b7d333ade3e5a00372dc1ec92d
spflow <sean.pascoe@gmail.com>
2016-07-29 09:15:52 -0600

    refactor game logic, add set Border & card Reset

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var cards = createArray(8);
+var cardNumber = 8;
+var cards = createArray(cardNumber);
 var choice1;
 var choice2;
 
@@ -22,32 +23,37 @@ function createArray(arrLength) {
 }
 
 function cardSelect(clickElem) {
-  var numEl = clickElem.firstElementChild
-  var cardValue = numEl.innerHTML;
-  populateChoice(cardValue);
-  numEl.style.visibility = "visible";
-}
+  var numEl = clickElem.firstElementChild;
 
-function populateChoice(cardVal) {
-
-  if (!choice1) {
-    choice1 = cardVal;
-  } else {
-    choice2 = cardVal;
-    cardCompare(choice1, choice2);
+  //checks to see if card value is currently visable
+  if(numEl.style.visibility !== "visible") {
+    numEl.style.visibility = "visible";
+    if (!choice1) {
+      choice1 = clickElem;
+    } else {
+      choice2 = clickElem;
+      cardCompare(choice1, choice2);
+    }
   }
-  console.log(choice1);
-  console.log(choice2);
 }
 
 function cardCompare(ch1, ch2) {
-  if (ch1 === ch2) {
+  var ch1Val = ch1.firstElementChild.innerHTML;
+  var ch2Val = ch2.firstElementChild.innerHTML;
+
+  console.log(ch1Val);
+  console.log(ch2Val);
+
+  if (ch1Val === ch2Val) {
     console.log("Cards Equal!")
+    setCardBorder(ch1, ch2, "correct");
     resetChoices();
     //keep cards revealed
     //reset choices
   } else {
     console.log("Cards Not Equal!")
+    setCardBorder(ch1, ch2, "incorrect");
+    setTimeout(function() {resetCards(ch1, ch2)}, 2000);
     resetChoices();
     //hide cards again
     //reset choices to null
@@ -57,4 +63,27 @@ function cardCompare(ch1, ch2) {
 function resetChoices() {
   choice1 = "";
   choice2 = "";
+}
+
+function resetCards(ch1, ch2) {
+  var ch1NumEl = ch1.firstElementChild;
+  var ch2NumEl = ch2.firstElementChild;
+
+  setCardBorder(ch1, ch2);
+  ch1NumEl.style.visibility = "hidden";
+  ch2NumEl.style.visibility = "hidden";
+}
+
+function setCardBorder(ch1, ch2, result) {
+  if (result == "incorrect") {
+    ch1.style.border = ch2.style.border = "2px solid red";
+  } else if (result === "correct") {
+    ch1.style.border = ch2.style.border = "2px solid green";
+  } else {
+    ch1.style.border = ch2.style.border = "none"
+  }
+}
+
+function resetGame() {
+
 }
